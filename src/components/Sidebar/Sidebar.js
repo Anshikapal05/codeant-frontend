568da76eaeed9e97249a924969207dea44be7d7b
1908shital <1908shitalchaudhari@gmail.com>
2024-12-26 23:36:29 +0530

    responsive

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,6 +7,7 @@ import {
   faHouse,
   faPhone,
   faRightFromBracket,
+  faTimes
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
@@ -21,11 +22,45 @@ function Sidebar() {
 
   return (
     <div className="sidebar-container">
-      {/* Hamburger Icon */}
+      {/* Hamburger or Cross Icon */}
       <div className="hamburger-icon" onClick={toggleMenu}>
-        <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
       </div>
-
+      
+      {/* Sidebar Menu */}
+      <div className={`sidebar `}>
+        <div className="profile-dropdown">
+          <select>
+            <option value="user1">UtkarshDhairyaPanwar</option>
+            <option value="user2">Other User</option>
+          </select>
+        </div>
+        <ul className="top-list">
+          <li>
+            <FontAwesomeIcon icon={faHouse} /> Repositories
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCode} /> AI Code Review
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCloud} /> Cloud Security
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBook} /> How to Use
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faGear} /> Settings
+          </li>
+        </ul>
+        <ul className="bottom-list">
+          <li>
+            <FontAwesomeIcon icon={faPhone} /> Support
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faRightFromBracket} /> Logout
+          </li>
+        </ul>
+      </div>
       {/* Mobile Menu */}
       {isMenuOpen && (
         <div className="mobile-menu">
